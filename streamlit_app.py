e54d1e431573e95164e203f6e62774fd0b2cf219
Daria Ralduhina <107572323+duhina@users.noreply.github.com>
2023-11-09 14:36:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit
 streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.header('Breakfast Menu2')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
